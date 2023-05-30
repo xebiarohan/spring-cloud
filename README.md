fdd4c1e1dbf3d16e2af795938c0fa65ef6178dfd
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2023-05-30 19:12:37 +0400

    Adding currency exchange rest API

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring-cloud
 Spring cloud practice
+
+
+
+URLs:
+
+Currency Exchange : http://localhost:8000/currency-exchange/from/USD/to/INR
+
---
+++ currency-exchange-service/src/main/java/com/rohan/springcloud/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,24 @@
+package com.rohan.springcloud.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000l, from, to, BigDecimal.valueOf(65));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
